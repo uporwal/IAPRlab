2c4a56e296953cb965e170d7cf4f75efc433abff
utkarsh <utkarsh.porwal@gmail.com>
2017-01-27 16:48:07 -0800

    updating readme

--- server.py
+++ server.py
@@ -4,27 +4,20 @@ import string
 import json
 import requests
 
-
-
 app = Flask(__name__)
-
 invalidChars = set(string.punctuation)
 
 def searchPrefixES(prefix):
-
 
     #1. Construct json query
     #2. POST query to the server
     #3. Process the response
     #4. Return a list of suggestions
-    
-
 
 @app.route('/')
 def home():
     html = render_template('home.html', name="IAPR AutoComplete Demo")
     return html
-
 
 @app.route('/searchPrefix')
 def searchPrefix():
@@ -32,7 +25,6 @@ def searchPrefix():
     prefix = query.split(" ")[-1]
     top10 = searchPrefixES(prefix)
     return jsonify({"prediction": top10})
-
 
 if __name__ == '__main__':
     app.run()
